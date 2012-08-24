0caa4377aba92db9809bab8263cc8f114055523b
bruno <bruno.bonacci@gmail.com>
2012-08-24 23:43:09 +0100

    added VERTX_HOME

--- mk.sh
+++ mk.sh
@@ -2,4 +2,4 @@
 
 rm -fr output
 mkdir output
-javac -cp $( echo `find /opt/vertx/lib/ -name \*.jar` | sed 's/ /:/g' ) $(find src/java -name \*.java) -d output
+javac -cp $( echo `find $VERTX_HOME/lib/ -name \*.jar` | sed 's/ /:/g' ) $(find src/java -name \*.java) -d output
